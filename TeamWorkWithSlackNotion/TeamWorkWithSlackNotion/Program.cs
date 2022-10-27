b8f1420bc2ad0a5f9384bd876132d360b52a2013
yosublee0223 <yosublee0223@gmail.com>
2022-10-27 19:15:20 +0900

    Commit

---
+++ TeamWorkWithSlackNotion/TeamWorkWithSlackNotion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamWorkWithSlackNotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello TeamWork");
+            Console.WriteLine("I'd like to connect this to not only Github but, also Slack and Notion if possible");
+        }
+    }
+}
